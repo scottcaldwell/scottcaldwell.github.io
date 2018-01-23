615e4f077943795f1a5f89ad1b1ab36ce911baf5
Scott Caldwell <scottc.caldwell@gmail.com>
2018-01-22 17:05:02 -0800

    added image galleries for each project, completed description and added tags

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function getRandomNumber(min, max) {
     return Math.trunc(Math.random() * (max - min) + min);
 }
 
-
 function createTrapezoid(initX, initY) {
     var canvas = document.getElementById('canvas');
     canvas.width = window.innerWidth;
@@ -42,27 +41,67 @@ function createTrapezoid(initX, initY) {
             ctx.closePath();
             ctx.fill();
             x += getRandomNumber(50, 100);
-
         }
-
-
     }
 }
 
 
 // createTrapezoid(0, 0);
-
-
-// x, y -> x+r, y
-// x+r, y -> x+r, y+r2
-// x+r, y+r2 -> x, y+r3
-
 //function createShape(opacity)
-
-
 //loop
 //create row of shapes, with similar opacity range
 
+
+
+//Image galleries
+var allImages = {
+    csImagesArray : ["cameronsparling_grid.png", "cameronsparling_full.png", "cameronsparling_contact.png", "cameronsparling_mobile.png"],
+    nhlImagesArray : ["nhlrecap_full.png", "nhlrecap_hidden.png", "nhlrecap_mobile.png", "nhlrecap_modal.png"],
+    pomoImagesArray : ["pomo_init.png", "pomo_additional_time.png"],
+    syncImagesArray : ["syncline_login.png", "syncline_landing.png", "syncline_sites.png", "syncline_location.png", "syncline_drill_hole.png"]
+}
+
+
+//I should probably combine these functions into one (changeImage), since only one line of code is different
+function nextImage(imageId, imageArray) {
+    var image = document.getElementById(imageId);
+    var currentIndex = image.dataset.index;
+    var newIndex = (parseInt(currentIndex) + 1) >= imageArray.length ? 0 : parseInt(currentIndex) + 1;
+    image.src = "img/" + imageArray[newIndex];
+    image.dataset.index = newIndex;
+}
+
+function prevImage(imageId, imageArray) {
+    var image = document.getElementById(imageId);
+    var currentIndex = image.dataset.index;
+    var newIndex = (parseInt(currentIndex) - 1) < 0 ? imageArray.length - 1 : parseInt(currentIndex) - 1;
+    image.src = "img/" + imageArray[newIndex];
+    image.dataset.index = newIndex;
+}
+
+
+var nextImageElements = document.getElementsByClassName("next-image");
+var prevImageElements = document.getElementsByClassName("prev-image");
+
+for(var i = 0; i < nextImageElements.length; i++){
+    nextImageElements[i].addEventListener('click', function (a) {
+        //there has to be a better way to do this. I suppose that's where jQuery excels at DOM traversal
+        //if I change the html structure this will break.
+        var imageId = this.previousElementSibling.childNodes[1].childNodes[1].id;
+        var imageArray = allImages[ imageId + "Array"];
+        nextImage(imageId, imageArray);
+    }, false);
+}
+
+for(var i = 0; i < prevImageElements.length; i++){
+    prevImageElements[i].addEventListener('click', function (e) {
+        var imageId = this.nextElementSibling.childNodes[1].childNodes[1].id;
+        var imageArray = allImages[ imageId + "Array"];
+        prevImage(imageId, imageArray);
+    }, false);
+}
+
+//Hamburger menu(code snippet from Bulma docs)
 document.addEventListener('DOMContentLoaded', function () {
 
     // Get all "navbar-burger" elements
